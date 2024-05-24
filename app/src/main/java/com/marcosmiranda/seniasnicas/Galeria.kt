29234baa490b5a815a12dc6dd6181f1b4aa1954f
Marcos <marcos@formacionytecnologia.com>
2024-05-24 14:32:38 +0200

    Create word images programatically

--- app/src/main/java/com/marcosmiranda/seniasnicas/Galeria.kt
+++ app/src/main/java/com/marcosmiranda/seniasnicas/Galeria.kt
@@ -1,37 +1,33 @@
 package com.marcosmiranda.seniasnicas
 
 import android.app.Activity
-import android.graphics.drawable.Drawable
-import android.media.MediaPlayer
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.media.PlaybackParams
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
 import android.view.View
 import android.widget.ImageView
 import android.widget.MediaController
 import android.widget.TextView
 import android.widget.VideoView
+import androidx.core.graphics.drawable.toBitmap
 import androidx.room.Room
 import com.google.android.material.switchmaterial.SwitchMaterial
-
 
 class Galeria : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_galeria)
 
-        val assets = applicationContext.assets
-
         val letraId = intent.getIntExtra("letra", 0)
         val palabraId = intent.getIntExtra("palabra", 0)
-        Log.e("letra", letraId.toString())
-        Log.e("palabra", palabraId.toString())
 
         var tvGaleriaText = ""
         var resName = ""
+        var imgBitmap: Bitmap? = null
 
         val tvGaleria = findViewById<TextView>(R.id.activity_galeria_tv_galeria)
         val ivGaleria = findViewById<ImageView>(R.id.activity_galeria_iv_galeria)
@@ -58,6 +54,7 @@ class Galeria : Activity() {
 
             tvGaleriaText = letraTxt
             resName = "letras_${letraTxtLower}"
+            imgBitmap = resources.getDrawable(resources.getIdentifier(resName, "drawable", packageName)).toBitmap()
         } else if (palabraId != 0) {
             val palabra = db.palabraDao().get(palabraId)
             val palabraTxt = palabra.texto
@@ -67,10 +64,11 @@ class Galeria : Activity() {
 
             tvGaleriaText = palabraTxt
             resName = "${categoriaTxtLower}_${palabraTxtLower}"
+            imgBitmap = createWordImage(palabraTxtLower)
         }
 
         tvGaleria.text = tvGaleriaText
-        ivGaleria.setImageResource(resources.getIdentifier(resName, "drawable", packageName))
+        ivGaleria.setImageBitmap(imgBitmap)
         vvGaleria.setVideoURI(Uri.parse("android.resource://$packageName/" + resources.getIdentifier(resName, "raw", packageName)))
 
         swGaleria.setOnCheckedChangeListener { _, checked ->
@@ -86,5 +84,127 @@ class Galeria : Activity() {
                 swGaleria.text = getString(R.string.galeria_imagen)
             }
         }
+    }
+
+    private fun createWordImage(word: String = "hola"): Bitmap {
+        val letterBitmapsList = mutableListOf<Bitmap>()
+        val wordBitmapRowsList = mutableListOf<Bitmap>()
+        var wordBitmapWidth = 0
+        var wordBitmapHeight = 500
+        var wordBitmapMaxWidth = 0
+        val wordBitmapRowHeight = 500
+        val wordBitmapWidthMargin = 100
+        val wordBitmapHeightMargin = 100
+        val wordBitmapConf = Bitmap.Config.ARGB_8888
+
+        // Handle special cases: special characters and compound letters (ch, ll, rr)
+        val wordChars = word.toCharArray()
+        val wordArr = mutableListOf<String>()
+        var prevChr = ' '
+        wordChars.forEach { chr ->
+            if (chr == '¿' || chr == '?' || chr == '¡' || chr == '!') return@forEach
+
+            if ((prevChr == 'c' && chr == 'h') ||
+                (prevChr == 'l' && chr == 'l') ||
+                (prevChr == 'r' && chr == 'r')) {
+                wordArr.removeLast()
+                wordArr.add("$prevChr$chr")
+            } else {
+                wordArr.add("$chr")
+            }
+
+            prevChr = chr
+        }
+
+        // For each letter, load corresponding bitmap
+        wordArr.forEach { chr ->
+            val imgBitmap: Bitmap?
+            if (chr == " ") {
+                imgBitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
+                if (wordBitmapWidth > wordBitmapMaxWidth) wordBitmapMaxWidth = wordBitmapWidth
+                wordBitmapWidth = 0
+                // wordBitmapHeight += wordBitmapRowHeight + wordBitmapHeightMargin
+            } else {
+                val drawableName = "letras_" + when (chr) {
+                    "á"     -> "a"
+                    "é"     -> "e"
+                    "í"     -> "i"
+                    "ó"     -> "o"
+                    "ú"     -> "u"
+                    "ñ"     -> "enie"
+                    else    -> chr
+                }
+
+                val imgDrawableId = resources.getIdentifier(drawableName, "drawable", packageName)
+                imgBitmap = resources.getDrawable(imgDrawableId).toBitmap()
+            }
+            letterBitmapsList.add(imgBitmap)
+            wordBitmapWidth += imgBitmap.width + wordBitmapWidthMargin
+        }
+
+        if (wordBitmapWidth > wordBitmapMaxWidth) wordBitmapMaxWidth = wordBitmapWidth
+        wordBitmapMaxWidth -= wordBitmapWidthMargin
+
+        // Draw all bitmaps in rows
+        var wordBitmapRow = Bitmap.createBitmap(wordBitmapMaxWidth, wordBitmapRowHeight, wordBitmapConf)
+        var wordCanvasRow = Canvas(wordBitmapRow)
+        var rowCurrentLeft = 0
+        var rowCurrentTop = 0
+        var rowCurrentBottom = wordBitmapRowHeight
+        letterBitmapsList.forEach { ltrImg ->
+            val ltrImgWidth = ltrImg.width
+            val ltrImgHeight = ltrImg.height
+            val newLine = ltrImgWidth == 100 && ltrImgHeight == 100
+            val lastLetter = ltrImg == letterBitmapsList.last()
+
+            val imgDestRect = Rect(rowCurrentLeft, rowCurrentTop, ltrImgWidth + rowCurrentLeft, rowCurrentBottom)
+            wordCanvasRow.drawBitmap(ltrImg, null, imgDestRect, null)
+            rowCurrentLeft += wordBitmapWidthMargin
+            if (!newLine) rowCurrentLeft += ltrImgWidth
+
+            if (newLine || lastLetter) {
+                // Clip bitmap and add to list
+                rowCurrentLeft -= wordBitmapWidthMargin
+                if (newLine) rowCurrentLeft -= ltrImgWidth
+                wordBitmapRow = Bitmap.createBitmap(wordBitmapRow, 0, 0, rowCurrentLeft, wordBitmapRow.height)
+                wordBitmapRowsList.add(wordBitmapRow)
+
+                // Reset
+                wordBitmapRow = Bitmap.createBitmap(wordBitmapMaxWidth, wordBitmapRowHeight, wordBitmapConf)
+                wordCanvasRow = Canvas(wordBitmapRow)
+                rowCurrentLeft = 0
+                rowCurrentTop = 0
+                rowCurrentBottom = wordBitmapRowHeight
+
+                // currentLeft = 0
+                // currentTop += wordBitmapRowHeight + wordBitmapHeightMargin
+                // currentBottom += wordBitmapRowHeight + wordBitmapHeightMargin
+                return@forEach
+            }
+        }
+
+        // Draw all rows
+        if (wordBitmapRowsList.count() > 1) {
+            wordBitmapHeight = (wordBitmapHeight + wordBitmapHeightMargin) * wordBitmapRowsList.count()
+            wordBitmapHeight -= wordBitmapHeightMargin
+        }
+        val wordBitmap = Bitmap.createBitmap(wordBitmapMaxWidth, wordBitmapHeight, wordBitmapConf)
+        val wordCanvas = Canvas(wordBitmap)
+        var currentLeft = 0
+        var currentTop = 0
+        var currentBottom = wordBitmapRowHeight
+        wordBitmapRowsList.forEach { rowImg ->
+            val rowImgWidth = rowImg.width
+            // val rowImgHeight = rowImg.height
+
+            // Center row if width is not max
+            if (rowImgWidth < wordBitmapMaxWidth) currentLeft = (wordBitmapMaxWidth - rowImgWidth) / 2
+            val imgDestRect = Rect(currentLeft, currentTop, rowImgWidth + currentLeft, currentBottom)
+            wordCanvas.drawBitmap(rowImg, null, imgDestRect, null)
+            currentTop += wordBitmapRowHeight + wordBitmapHeightMargin
+            currentBottom += wordBitmapRowHeight + wordBitmapHeightMargin
+        }
+
+        return wordBitmap
     }
 }
