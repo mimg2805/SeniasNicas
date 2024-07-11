6699e6559b5a73b96e32a80724bbe54e17de7be8
Marcos <marcos@formacionytecnologia.com>
2024-07-11 17:44:47 +0200

    Lista de Juegos

---
+++ app/src/main/java/com/marcosmiranda/seniasnicas/Juegos.kt
@@ -0,0 +1,11 @@
+package com.marcosmiranda.seniasnicas
+
+import android.app.Activity
+import android.os.Bundle
+
+class Juegos : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juegos)
+    }
+}
--- app/src/main/java/com/marcosmiranda/seniasnicas/MainMenu.kt
+++ app/src/main/java/com/marcosmiranda/seniasnicas/MainMenu.kt
@@ -2,6 +2,7 @@ package com.marcosmiranda.seniasnicas
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 
@@ -16,8 +17,11 @@ class MainMenu : Activity() {
         val btnDiccionario = findViewById<Button>(R.id.activity_main_menu_btn_diccionario)
         btnDiccionario.setOnClickListener { startActivity(Intent(this, Diccionario::class.java)) }
 
-        val btnHistoria = findViewById<Button>(R.id.activity_main_menu_btn_historia)
+        // val btnHistoria = findViewById<Button>(R.id.activity_main_menu_btn_historia)
         // btnHistoria.setOnClickListener { startActivity(Intent(this, Historia::class.java)) }
+
+        val btnJuegos = findViewById<Button>(R.id.activity_main_menu_btn_juegos)
+        btnJuegos.setOnClickListener { startActivity(Intent(this, Juegos::class.java)) }
 
         val btnMasApps = findViewById<Button>(R.id.activity_main_menu_btn_mas_apps)
         btnMasApps.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.developer_url)))) }
